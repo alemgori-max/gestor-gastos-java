5b5305a009a4317aef21e0b42b3ccce302684220
alem <alemgori@gmail.com>
2026-08-24 23:41:31 -0600

    Agrega título del estudiante A

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("CONTROL PERSONAL DE GASTOS");
+        System.out.println("CONTROL PERSONAL DE LOS GASTOS");
 
 
         ArrayList<String> conceptos = new ArrayList<>();
